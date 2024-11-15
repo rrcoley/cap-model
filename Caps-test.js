0ea0458b77ae00aefbaf879803044c9bb778a188
rrcoley <66431497+rrcoley@users.noreply.github.com>
2024-11-15 08:01:45 +0000

    Add files via upload

---
+++ Caps-test.js
@@ -0,0 +1,12 @@
+CapModel = [
+		{ 
+			"level": "L1",
+			"title": "End User Compute and Business Applications",
+			"color": "purple",
+		},
+		{ 
+			"level": "L1",
+			"title": "Event and Messaging",
+			"color": "red",
+		}
+	]
